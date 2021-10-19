19dc301b881d8691b8546a0eed2ec8c9002b6354
Franckino <franckinokand@gmail.com>
2021-10-20 00:05:58 +0200

    initial commit

---
+++ src/main/java/com/franckino/App.java
@@ -0,0 +1,13 @@
+package com.franckino;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
